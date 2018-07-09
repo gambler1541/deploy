3f53ce3e5c2135b3b515eb6541d43fe5e67f4aa1
pmb <gambler1541@gmail.com>
2018-07-09 14:45:42 +0900

    docker에서도 이미지 변경

--- app/members/admin.py
+++ app/members/admin.py
@@ -4,10 +4,18 @@ from .models import User
 
 class UserAdmin(BaseUserAdmin):
     fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        (('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
-        (('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
-                                       'groups', 'user_permissions')}),
-        (('Important dates'), {'fields': ('last_login', 'date_joined')}),
+        (None, {
+            'fields': (
+                'username',
+                'password',
+
+            ),
+        }),
+        ('개인정보', {
+            'fields': (
+                'img_profile',
+
+            ),
+        }),
     )
 admin.site.register(User,UserAdmin)
